37b97e5f2a83f7716b7811599d9763d6ff969200
laslil7437 <laslil7437@users.noreply.github.com>
2024-06-14 12:52:21 +0000

    draft 2

--- src/data/utils.py
+++ src/data/utils.py
@@ -7,6 +7,7 @@ from .wikitext import get_wikitext_data
 from .arxiv import get_arxiv_2000, get_arxiv_full
 from .openwebtext2 import get_openwebtext2_data
 from .slimpajama import get_slimpajama_data
+from .fineweb import get_fineweb10_data
 
 
 def get_dataset(args) -> Dict[str, np.ndarray]:
@@ -31,6 +32,8 @@ def get_dataset(args) -> Dict[str, np.ndarray]:
         return get_openwebtext2_data()
     if args.dataset == "slimpajama":
         return get_slimpajama_data()
+    if args.dataset == "fineweb":
+        return get_fineweb10_data()
     else:
         raise NotImplementedError(f"Unknow dataset key '{args.dataset}'")
 
