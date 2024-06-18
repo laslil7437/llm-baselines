3b41c028826c4f6bebe421404e3b353a7e0118c4
laslil7437 <laslil7437@users.noreply.github.com>
2024-06-18 12:02:11 +0000

    FW10 tokenized for first trainingn pass.

--- src/data/fineweb10.py
+++ src/data/fineweb10.py
@@ -25,6 +25,11 @@ def get_fineweb10_data(num_proc=40):
         split_dataset["val"] = split_dataset.pop("test")
         print("----> dataset split")
 
+        seq_length = 512
+        # special token for each dump
+        dump_tokens = set(example["dump"] for example in dataset["train"])
+        dump_token_ids = {token: i+50256+1 for i, token in enumerate(dump_tokens)}
+
         def process(example):
             ids = tknzr.encode_ordinary(
                 example["text"]
@@ -32,69 +37,57 @@ def get_fineweb10_data(num_proc=40):
             ids.append(
                 tknzr.eot_token
             )  # add the end of text token, e.g. 50256 for gpt2 bpe
-            out = {"ids": ids, "len": len(ids), "dump": example["dump"]}
-            return out
 
-        
+            # insert dump_token_id every 512 tokens
+            dump_token_id = dump_token_ids[example["dump"]]
+            for i in range(0, len(ids), seq_length):
+                ids.insert(i, dump_token_id)
+
+            out = {"ids": ids, "len": len(ids)}
+            return out
 
         # tokenize the dataset
         tokenized = split_dataset.map(
             process,
-            remove_columns=["text"],
+            remove_columns=["text", "dump"],
             desc="tokenizing the splits",
             num_proc=num_proc,
         )
+        print("----> dataset tokenized")
 
-        all_train_data = os.path.join(FINEWEB10_DATA_PATH, "train.bin")
-        all_val_data = os.path.join(FINEWEB10_DATA_PATH, "val.bin")
+        
         # concatenate all the ids in each dataset into one large file we can use for training
         for split, dset in tokenized.items():
+            arr_len = np.sum(dset["len"])
+            filename = os.path.join(FINEWEB10_DATA_PATH, f"{split}.bin")
+            dtype = np.uint16  # (can do since enc.max_token_value == 50256 is < 2**16)
+            arr = np.memmap(filename, dtype=dtype, mode="w+", shape=(arr_len,))
+            total_batches = min(1024, len(dset))
 
-            dset_per_dump = dset.to_polars().groupby('dump')
-            for dump, dset_dump in dset_per_dump:
+            idx = 0
+            for batch_idx in tqdm(range(total_batches), desc=f"writing {filename}"):
+                # Batch together samples for faster write
+                batch = dset.shard(
+                    num_shards=total_batches, index=batch_idx, contiguous=True
+                ).with_format("numpy")
+                arr_batch = np.concatenate(batch["ids"])
+                # Write into mmap
+                arr[idx : idx + len(arr_batch)] = arr_batch
+                idx += len(arr_batch)
+            arr.flush()
+        
+    train_data = np.memmap(
+        os.path.join(FINEWEB10_DATA_PATH, "train.bin"), dtype=np.uint16, mode="r"
+    )
+    val_data = np.memmap(
+        os.path.join(FINEWEB10_DATA_PATH, "val.bin"), dtype=np.uint16, mode="r"
+    )
 
-                dset_dump_pd = dset_dump.to_pandas()
-                dset_dump = Dataset.from_pandas(dset_dump_pd)
+    return {'train': train_data, 'val': val_data}
 
-                arr_len = np.sum(dset_dump["len"]) # number of tokens in a given dump
-
-                foldername = os.path.join(FINEWEB10_DATA_PATH, dump)
-                os.makedirs(foldername, exist_ok=True)
-                filename = os.path.join(foldername, f"{split}.bin")
-                # filename = os.path.join(FINEWEB10_DATA_PATH, f"{dump}_{split}.bin")
-
-                dtype = np.uint16  # (can do since enc.max_token_value == 50256 is < 2**16)
-                arr = np.memmap(filename, dtype=dtype, mode="w+", shape=(arr_len,))
-                total_batches = min(1024, len(dset_dump))
-
-                idx = 0
-                for batch_idx in tqdm(range(total_batches), desc=f"writing {filename}"):
-                    # Batch together samples for faster write
-                    batch = dset_dump.shard(
-                        num_shards=total_batches, index=batch_idx, contiguous=True
-                    ).with_format("numpy")
-                    arr_batch = np.concatenate(batch["ids"])
-                    # Write into mmap
-                    arr[idx : idx + len(arr_batch)] = arr_batch
-                    idx += len(arr_batch)
-                arr.flush()
-
-                # add entry to concatenated train.bin and val.bin
-                if filename.endswith("train.bin"):
-                    with open(all_train_data, 'ab') as outfile:
-                        with open(os.path.join(FINEWEB10_DATA_PATH, filename), 'rb') as infile:
-                            shutil.copyfileobj(infile, outfile)
-                elif filename.endswith("val.bin"):
-                    with open(all_val_data, 'ab') as outfile:
-                        with open(os.path.join(FINEWEB10_DATA_PATH, filename), 'rb') as infile:
-                            shutil.copyfileobj(infile, outfile)
-    
-        train_data = np.memmap(all_train_data, dtype=np.uint16, mode="r")
-        val_data = np.memmap(all_val_data, dtype=np.uint16, mode="r")
-
-        breakpoint()
-        return {'train': train_data, 'val': val_data}
-    
-
-# 9,318,992,613 training tokens and 1,036,331,430 validation tokens 
+# 9,318,992,613 training tokens and 1,036,331,430 validation tokens = 10,355,324,043 total tokens (without counting dump tokens)
+# 9,344,323,311 training tokens and 1,039,147,285 validation tokens = 10,383,470,596 total tokens (counting dump tokens)
+# Note: 
+# 10,383,470,596 tokens / 512 tokens per sequence = 20,263,476 sequences
+#
 # 95 dumps
